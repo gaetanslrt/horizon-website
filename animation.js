d56c7c310f803a710c18fc1b9b801776e735c2e8
gaetanslrt <gaetan.slt@gmail.com>
2025-02-24 21:12:40 +0100

    Add animation more

--- animation.js
+++ animation.js
@@ -1,9 +1,20 @@
     document.addEventListener("DOMContentLoaded", function () {
         const text = "Stay ahead of the storm with Horizon.";
         const speed = 45; 
-        const startDelay = 1400; 
+        const startDelay = 1350; 
         let i = 0;
         const typewriter = document.querySelector(".typewriter");
+        const dwnb = document.querySelector(".download-btn");
+        dwnb.style.visibility = "hidden";
+
+        function show() {
+            setTimeout(() => {
+                dwnb.style.transition = "opacity 0.2s linear";
+                dwnb.style.visibility = "visible";
+                dwnb.style.opacity = 1;
+            }, 4500);
+        }
+        dwnb.style.opacity = 0;
 
         function type() {
             if (i < text.length) {
@@ -19,4 +30,5 @@
 
         
         setTimeout(type, startDelay);
+        show();
     })
